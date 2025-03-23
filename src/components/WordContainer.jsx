09560634b7c422c8b9d90aea71dedd8aa987d4b9
Kálmán Inez <inez.kalman@qulto.eu>
2025-03-23 22:41:30 +0100

    Create base project
    To install everything, run "yarn install"

---
+++ src/components/WordContainer.jsx
@@ -0,0 +1,15 @@
+
+import SvgWorld from './SvgWorld'
+
+function App() {
+
+  return (
+    <div className="world-container">
+
+    <SvgWorld height={400} width={800} className="world"/>
+
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+import './assets/styles/index.scss';
+import WordContainer from './components/WordContainer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WordContainer />
   </StrictMode>,
 )
