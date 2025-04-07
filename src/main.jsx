c3167566b85a8b600ca99332bbf79535ab1a26fe
Huszics Peter <huszicspeter@gmail.com>
2025-04-07 20:54:58 +0200

    Started work with the react-simple-maps library

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 
 import './assets/styles/index.scss';
 import WordContainer from './components/WordContainer.jsx'
+import MapRenderer from './components/mapRenderer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WordContainer />
+    <MapRenderer />
   </StrictMode>,
 )
