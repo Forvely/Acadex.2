044bb20f59918714c50813505c9c7cd8de0a6131
Huszics Peter <huszicspeter@gmail.com>
2025-05-04 10:31:38 +0200

    map-package

--- src/components/mapRenderer.jsx
+++ src/components/mapRenderer.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useCallback } from "react";
-import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, ZoomableGroup, Marker } from "react-simple-maps";
 import { geoRobinson } from "d3-geo-projection";
 import { geoCentroid } from "d3-geo";
-import topojson from 'topojson-client' // topojson -> geojson for easier coordinate use
+import * as topojson from "topojson-client"; // topojson -> geojson for easier coordinate use
 
 // to do: implement the topojson-client into the website and use the coordinates instead of viewbox to zoom into the countries 
 
 const ViewBox_Width = 2000;
 const ViewBox_Height = 1000;
 const defaultViewBox = "0 0 2000 1000 "
+
+const topoMap = "/countries-topojson.json"
+
 
 export default function MapChart() {
 
@@ -21,18 +24,22 @@ export default function MapChart() {
   const [position, setPosition] = useState({
     coordinates: [0, 0],
     zoom: 1,
-    viewBox: defaultViewBox
   });
+
+  const [markerCoords, setMarkerCoords] = useState(null);
   
   // Compute centroid on click and zoom in
   const handleContinentClick = useCallback((geo) => {
     const [lon, lat] = geoCentroid(geo);
     setPosition({ coordinates: [lon, lat], zoom: 4 });
+    setMarkerCoords([lon, lat]);
+    console.log([lon, lat])
   }, []);
 
   // Reset to full-world view
   const handleReset = () => {
-    setPosition({ viewBox: defaultViewBox });
+    setPosition({ zoom: 1, coordinates: [0,0] });
+    setMarkerCoords([0, 0]);
   };
 
   return (
@@ -41,14 +48,14 @@ export default function MapChart() {
         Reset View
       </button>
 
-      <ComposableMap projection={projection} viewBox={defaultViewBox} preserveAspectRatio="xMinYMin meet" style={{ width: "100%", height: "auto" }}>
+      <ComposableMap projection={projection}  preserveAspectRatio="xMinYMin meet" style={{ width: "100%", height: "auto" }}>
         <ZoomableGroup
           center={position.coordinates}
           zoom={position.zoom}
           // Reject all user drag or wheel events
           filterZoomEvent={() => false}
         >
-          <Geographies geography="/countries-topojson.json">
+          <Geographies geography={topoMap}>
             {({ geographies }) =>
               geographies.map((geo) => (
                 <Geography
@@ -64,6 +71,11 @@ export default function MapChart() {
               ))
             }
           </Geographies>
+          {markerCoords && (
+            <Marker coordinates={markerCoords}>
+              <circle r={5} fill="red" stroke="#fff" strokeWidth={1} />
+            </Marker>
+          )}
         </ZoomableGroup>
       </ComposableMap>
     </div>
